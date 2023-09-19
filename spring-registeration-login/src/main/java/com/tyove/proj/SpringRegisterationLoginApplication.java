8d0233480cff5cdf0faed516908e311638a0a0a6
TylerOvenden <tovenden4386@bths.edu>
2023-09-19 00:10:19 -0400

    started creating login & registeration page with SpringBoot

---
+++ spring-registeration-login/src/main/java/com/tyove/proj/SpringRegisterationLoginApplication.java
@@ -0,0 +1,13 @@
+package com.tyove.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRegisterationLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRegisterationLoginApplication.class, args);
+	}
+
+}
